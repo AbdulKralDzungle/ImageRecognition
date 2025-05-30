b48ae15ce71c2f1f77af94324e45c1110ff7ca77
damia <dambibimbus@gmail.com>
2025-05-30 19:25:41 +0200

    UI cleanup(another one, java fx i weird)

--- src/main/HelloApplication.java
+++ src/main/HelloApplication.java
@@ -9,10 +9,90 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.awt.*;
-import java.io.IOException;
 
-public class HelloApplication{
+public class HelloApplication extends Application {
+    private int[][] grid;
+    private Rectangle[][] rectangle;
+    private Scene scene1;
+    private Button restartBt;
+    private final int gridSize = 28;
+    private final int cellSize = 10;
+    private Pane pane;
+
     public static void main(String[] args) {
-        Ui ui = new Ui();
+        launch(args);
+
+        //Ui ui = new Ui();
+    }
+
+    private void initialize() {
+
+        grid = new int[28][28];
+        rectangle = new Rectangle[28][28];
+        pane = new Pane();
+        scene1 = new Scene(pane, 320, 400);
+        restartBt = new Button("Restart");
+
+        restartBt.setScaleX(1);
+        restartBt.setScaleY(1);
+        restartBt.setLayoutY(320);
+
+        for (int i = 0; i < gridSize; i++) {
+            for (int j = 0; j < gridSize; j++) {
+                Rectangle r = new Rectangle();
+                r.setHeight(cellSize);
+                r.setWidth(cellSize);
+                r.setX(i * (cellSize + 1));
+                r.setY(j * (cellSize + 1));
+
+                rectangle[i][j] = r;
+                pane.getChildren().add(rectangle[i][j]);
+            }
+        }
+    }
+
+    private void addActions() {
+        pane.setOnMouseDragged(e -> {
+            Point pointerLocation = MouseInfo.getPointerInfo().getLocation();
+
+            int sceneX = pointerLocation.x;
+            sceneX -= (int) pane.getScene().getWindow().getX();
+            sceneX -= (int) pane.getScene().getX();
+
+            int sceneY = pointerLocation.y;
+            sceneY -= (int) pane.getScene().getWindow().getY();
+            sceneY -= (int) pane.getScene().getY();
+
+            int x = (int) Math.floor((double) sceneX / (cellSize + 1));
+            int y = (int) Math.floor((double) sceneY / (cellSize + 1));
+            try {
+                rectangle[x][y].setFill(javafx.scene.paint.Color.WHITE);
+                grid[x][y] = 256;
+            } catch (Exception _) {
+
+            }
+
+        });
+        restartBt.setOnAction(e -> {
+            grid = new int[28][28];
+            for (int i = 0; i < gridSize; i++) {
+                for (int j = 0; j < gridSize; j++) {
+                    rectangle[i][j].setFill(Color.BLACK);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        initialize();
+        addActions();
+        stage.setTitle("Ban Dlazek");
+        stage.setScene(scene1);
+        stage.setResizable(false);
+        //------------------------------------
+        pane.getChildren().add(restartBt);
+        stage.show();
+        //------------------------------------
     }
 }
--- src/main/controol/NetworkThred.java
+++ src/main/controol/NetworkThred.java
@@ -1,18 +1,42 @@
 package main.controol;
 
+import main.network.DataReader;
+import main.network.Network;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class NetworkThred implements Runnable {
     private HashMap<String, Command> commands;
     private String commandText;
+    private double learningRate;
     private Command command;
+    private ArrayList<Integer> inputs;
+    //----------------------------------
+    DataReader reader;
+    Network nt;
+
+    //----------------------------------
 
     public NetworkThred() {
         initialize();
     }
 
+    public void setInputs(int[][] inputs) {
+        this.inputs.clear();
+        for (int[] i : inputs) {
+            for (int j : i) {
+                this.inputs.add(j);
+            }
+        }
+    }
+
     private void initialize() {
+        learningRate = 0.1;
+        nt = new Network(50, 784, learningRate, 2, 10);
+        inputs = new ArrayList<>();
         commands = new HashMap<>();
+        commandText = "tick";
         commands.put("train", new TrainNetwork());
         commands.put("tick", new NetworkTick());
         commands.put("load", new TrainNetwork());
@@ -24,6 +48,7 @@ public class NetworkThred implements Runnable {
     @Override
     public void run() {
         while (!command.exit()) {
+            command = commands.get(commandText);
             command.execute();
         }
     }
